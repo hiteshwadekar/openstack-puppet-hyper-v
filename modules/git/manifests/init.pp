ca4b7ff350dd2625ab16d1950a2900683fd4bf87
Peter J. Pouliot <ppouliot@microsoft.com>
2012-08-10 01:27:23 -0400

    adding git install to puppet

---
+++ modules/git/manifests/init.pp
@@ -0,0 +1,8 @@
+class git {
+
+    package { "git-1711":
+        source => 'http://code.google.com/p/msysgit/downloads/detail?name=Git-1.7.11-preview20120710.exe&can=2&q=full+installer+official+git',
+        provider => msi,
+        ensure   => installed,
+    }
+}
