8758995cf4be6bb272df0567682520d621d2d49f
Peter J. Pouliot <peter@pouliot.net>
2012-08-20 15:08:00 -0400

    changed feature name to Microsoft-Hyper-V

--- modules/hyper-v/manifests/add_hyper-v_feature.pp
+++ modules/hyper-v/manifests/add_hyper-v_feature.pp
@@ -1,6 +1,6 @@
 class hyper-v::add_hyper-v_feature {
   exec { 'hyperv':
-   command => 'C:\Windows\sysnative\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command {Add-WindowsFeature Hyper-V}',
+   command => 'C:\Windows\sysnative\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command {Add-WindowsFeature Microsoft-Hyper-V}',
   }
   exec { 'hyperv_mgmt':
    command => 'C:\Windows\sysnative\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command {Add-WindowsFeature RSAT-Hyper-V-Tools}',
