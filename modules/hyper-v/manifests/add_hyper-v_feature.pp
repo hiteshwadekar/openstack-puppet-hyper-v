f54c32be3c8eeac021434536279f30cf81672b4c
Peter J. Pouliot <peter@pouliot.net>
2012-08-20 15:34:18 -0400

    added path to add_hyper-v_feature.pp

--- modules/hyper-v/manifests/add_hyper-v_feature.pp
+++ modules/hyper-v/manifests/add_hyper-v_feature.pp
@@ -1,8 +1,10 @@
 class hyper-v::add_hyper-v_feature {
   exec { 'hyperv':
-   command => 'C:\Windows\sysnative\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command {Add-WindowsFeature Microsoft-Hyper-V}',
+   path => $winpath
+   command => 'C:\Windows\sysnative\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command { Add-WindowsFeature Hyper-V }',
   }
   exec { 'hyperv_mgmt':
-   command => 'C:\Windows\sysnative\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command {Add-WindowsFeature RSAT-Hyper-V-Tools}',
+   path => $winpath
+   command => 'C:\Windows\sysnative\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command { Add-WindowsFeature RSAT-Hyper-V-Tools }',
   }
 }
