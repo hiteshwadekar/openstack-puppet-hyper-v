4c1975aff9e71bc31945ab10a96746dabdc0b3da
Peter J. Pouliot <peter@pouliot.net>
2012-08-20 14:22:56 -0400

    changed System32 to sysnative

--- modules/hyper-v/manifests/add_hyper-v_feature.pp
+++ modules/hyper-v/manifests/add_hyper-v_feature.pp
@@ -1,8 +1,8 @@
 class hyper-v::add_hyper-v_feature {
   exec { 'hyperv':
-   command => 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command {Add-WindowsFeature Hyper-V}',
+   command => 'C:\Windows\sysnative\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command {Add-WindowsFeature Hyper-V}',
   }
   exec { 'hyperv_mgmt':
-   command => 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command {Add-WindowsFeature RSAT-Hyper-V-Tools}',
+   command => 'C:\Windows\sysnative\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command {Add-WindowsFeature RSAT-Hyper-V-Tools}',
   }
 }
