8bda8e4e641e231f4f09f90b51a27b30ecc7f474
Peter J. Pouliot <peter@pouliot.net>
2012-08-20 14:01:13 -0400

    fixed typo

--- modules/hyper-v/manifests/add_hyper-v_feature.pp
+++ modules/hyper-v/manifests/add_hyper-v_feature.pp
@@ -1,8 +1,8 @@
 class hyper-v::add_hyper-v_feature {
   exec { 'hyperv':
-   command => 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command {Install-WindowsFeature Hyper-V}',
+   command => 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command {Add-WindowsFeature Hyper-V}',
   }
   exec { 'hyperv_mgmt':
-   command => 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command {Install-WindowsFeature RSAT-Hyper-V-Tools}',
+   command => 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command {Add-WindowsFeature RSAT-Hyper-V-Tools}',
   }
 }
