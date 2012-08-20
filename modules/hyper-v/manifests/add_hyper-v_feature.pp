5db8b4be2dd539f4e9674cf67d587fb8769308ed
Peter J. Pouliot <peter@pouliot.net>
2012-08-20 15:36:10 -0400

    fixed typo

--- modules/hyper-v/manifests/add_hyper-v_feature.pp
+++ modules/hyper-v/manifests/add_hyper-v_feature.pp
@@ -1,10 +1,10 @@
 class hyper-v::add_hyper-v_feature {
   exec { 'hyperv':
-   path => $winpath
+   path => $winpath,
    command => 'C:\Windows\sysnative\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command { Add-WindowsFeature Hyper-V }',
   }
   exec { 'hyperv_mgmt':
-   path => $winpath
+   path => $winpath,
    command => 'C:\Windows\sysnative\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -Command { Add-WindowsFeature RSAT-Hyper-V-Tools }',
   }
 }
